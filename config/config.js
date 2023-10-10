6d00c79556a498b4563181ba03bc6e879f3d0f3d
aju-0 <ajunair0@gmail.com>
2023-10-10 11:50:46 +0530

    usm by ajay code

---
+++ config/config.js
@@ -0,0 +1,10 @@
+const sessionSecret = "mysitesessionsecret";
+
+const emailUser='!@#$!@#$%@#$%@gmail.com'
+const emailPassword='!@#$ !@#$ !@#$ aogy'
+
+module.exports ={
+   sessionSecret,
+   emailUser,
+   emailPassword
+}
